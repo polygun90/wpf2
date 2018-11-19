d2d70c06250907e52c3aa879af10878f3449b266
sungjun park <polygun90@naver.com>
2018-11-19 16:04:32 +0900

    샘플 테스트

---
+++ CompileXamlWindow/CompileXamlWindow/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CompileXamlWindow
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //필드초기화 후 이벤트 핸들러를 가지고 오기 위한 메소드 호출
+            InitializeComponent();
+            //브러시 이름으로 ListBox를 채움
+            foreach (PropertyInfo prop in typeof(Brushes).GetProperties())
+                lstbox.Items.Add(prop.Name);
+        }
+
+        private void ButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            MessageBox.Show(b.Content.ToString());
+        }
+
+        private void ListBoxOnSelection(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox listBox = sender as ListBox;
+            string items = listBox.SelectedItem as string;
+            PropertyInfo pInfo = typeof(Brushes).GetProperty(items);
+            elips.Fill = (Brush)pInfo.GetValue(null, null);
+        }
+    }
+}
