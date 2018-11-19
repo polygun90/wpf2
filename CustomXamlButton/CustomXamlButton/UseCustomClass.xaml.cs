e762eb9b326a46dce7162426cf6e7ccb84666ca7
sungjun park <polygun90@naver.com>
2018-11-19 16:34:02 +0900

    샘플 추가

---
+++ CustomXamlButton/CustomXamlButton/UseCustomClass.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CustomXamlButton
+{
+    /// <summary>
+    /// UseCustomClass.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class UseCustomClass : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new UseCustomClass());
+        }
+        public UseCustomClass()
+        {
+            InitializeComponent();
+
+            //버튼 2개를 추가로 생성
+            for (int i = 0; i < 2; i++)
+            {
+                CenteredButton b = new CenteredButton();
+                b.Content = "Button " + (i + 1);
+                mystack.Children.Add(b);
+            }
+        }
+    }
+}
---
+++ MyControl/MyControl/MyControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyControl
+{
+    /// <summary>
+    /// MyControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MyUserControl : UserControl
+    {
+        public MyUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = "You have just clicked the button";
+        }
+    }
+}
