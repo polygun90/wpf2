b8db3ce3d3061047857a0b33d8fd598010b27c7c
sungjun park <polygun90@naver.com>
2018-11-20 15:25:23 +0900

    샘플 추가

---
+++ WpfApplication9/WpfApplication9/Emp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication9
+{
+    public class Emp
+    {
+        public string Ename { get; set; }
+        public string City { get; set; }
+    }
+}
